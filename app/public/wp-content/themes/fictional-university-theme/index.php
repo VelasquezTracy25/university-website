74365a4b63d64edbd8a5214d3df83a3bb8632512
VelasquezTracy25 <velasqueztracy25@gmail.com>
2021-07-07 16:02:44 -0500

    Posts

---
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -0,0 +1,18 @@
+<?php 
+    $names = array('Brad','John','Jane', "Meowsalot");
+
+    $count = 0;
+
+    while($count < count($names)){
+        echo "<li>$names[$count]</li>";
+        $count++;
+    }
+
+    while(have_posts()){
+        // 
+        the_post(); ?>
+<h2><a href="<?php the_permalink()?>"><?php the_title();?></a></h2>
+<?php the_content();?>
+<hr>
+<?php }
+    ?>
---
+++ app/public/wp-content/themes/fictional-university-theme/single.php
@@ -0,0 +1,2 @@
+<h1><?php the_title(); ?></h1>
+<em><?php the_content(); ?></em>
